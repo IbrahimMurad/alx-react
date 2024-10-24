87ebc44f31c9f425e681c92882f1f4f011095168
IbrahimMurad <ibrahimmorad31@gmail.com>
2024-10-24 04:20:13 +0300

    multi entry points way

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, './js/dashboard_main.js'),
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js'),
+    },
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
